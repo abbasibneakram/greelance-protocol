2cca8d5918781ed2e06c94c86a305cc3438a7831
abbasibneakram <abbasibneakram@gmail.com>
2024-05-22 16:40:39 +0500

    added / endpoint

--- backend-apis/server.js
+++ backend-apis/server.js
@@ -11,6 +11,9 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 const PORT = process.env.PORT || 3000
+app.get('/', async (req, res) => {
+    res.send('Hello API')
+})
 app.use('/api', nftRoute)
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
