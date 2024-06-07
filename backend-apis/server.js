21a7e0ef69bee2c560e2320e7047e71a7a451e6c
abbasibneakram <abbasibneakram@gmail.com>
2024-06-07 16:23:18 +0500

    added cors origin

--- backend-apis/server.js
+++ backend-apis/server.js
@@ -1,21 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const ethers = require('ethers')
 const cors = require('cors')
 const dotenv = require('dotenv')
-
 dotenv.config()
-app.use(cors())
+
 const nftRoute = require('./routes/nftRoutes')
 
 const app = express()
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
 
 const PORT = process.env.PORT || 3000
 app.get('/', async (req, res) => {
     res.send('Hello API')
 })
+
+app.use(cors())
+app.use(cors({ origin: 'http://localhost:4300' }))
 app.use('/api', nftRoute)
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
