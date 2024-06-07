99312b4482f6bad6cccc34706064443e81ae4e27
abbasibneakram <abbasibneakram@gmail.com>
2024-06-07 15:51:46 +0500

    implemented cors

--- backend-apis/server.js
+++ backend-apis/server.js
@@ -1,24 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const ethers = require('ethers')
+const cors = require('cors')
 const dotenv = require('dotenv')
-dotenv.config()
 
+dotenv.config()
+app.use(cors())
 const nftRoute = require('./routes/nftRoutes')
 
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    )
-    next()
-})
 
 const PORT = process.env.PORT || 3000
 app.get('/', async (req, res) => {
